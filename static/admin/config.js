5bfb7af2c8c582ac20b1a16980344e808249380b
Ivan Krivosheev <i.krivosheev@cian.ru>
2017-12-16 20:04:29 +0300

    Add work with ckeditor in admin

--- static/admin/config.js
+++ static/admin/config.js
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Anchor,Underline,Strike,Outdent,Indent,Subscript,Superscript';
+	config.removeButtons = 'Cut,Copy,Paste,Anchor,Underline,Outdent,Indent,Strike,Subscript,Superscript';
 
 	// Dialog windows are also simplified.
 	config.removeDialogTabs = 'link:advanced';
