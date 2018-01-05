dae719b150cca3435bdffb34a47a1833e06c8a4c
Ivan Krivosheev <i.krivosheev@cian.ru>
2018-01-05 23:21:13 +0300

    Update build wheel file for project

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
+build-wheel:
+	python setup.py bdist_wheel --universal
 
-build:
+build-tar-gz:
 	python setup.py sdist
 
 clean:
 	rm -rf dist/ build/ Vaka_Avto.egg-info
 
-.PHONY: clean build
+.PHONY: clean build-tar-gz build-wheel
--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
 import glob
+import pathlib
+import collections
 
-from pip.req import parse_requirements
-from setuptools import setup
-from setuptools import find_packages
+import setuptools
+from pip import req
 
 
-setup(
+def get_recursive_files(src, dst):
+    data_files = collections.defaultdict(list)
+    for path in glob.glob('{}/**/*'.format(src), recursive=True):
+        path_obj = pathlib.Path(path)
+        if path_obj.is_file():
+            new_path = (dst, ) + path_obj.parent.parts[1:]
+            dirname = pathlib.Path(*new_path)
+            data_files[str(dirname)].append(path)
+
+    return tuple(data_files.items())
+
+
+setuptools.setup(
     name='Vaka-Avto',
-    version='0.0.1',
+    version='0.0.2',
     author="Ivan Krivosheev, Semen Doncu",
     author_email="py.krivosheev@gmail.com, doncusemen@gmail.com",
     description="Site for Vaka-Avto",
     url='http://vaka-avto.ru/',
     zip_safe=False,
     python_requires='>=3.4',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     include_package_data=True,
-    data_files=(
-        ('static', glob.glob('static/**/*', recursive=True) + ['static/robots.txt']),
-        ('configs', ['etc/uwsgi.ini', 'etc/supervisord.conf']),
-    ),
-    install_requires=[str(ir.req) for ir in parse_requirements('requirements.txt', session='hack')]
+    data_files=get_recursive_files('etc', 'etc') + get_recursive_files('static', 'static'),
+    install_requires=[str(ir.req) for ir in req.parse_requirements('requirements.txt', session='hack')]
 )
