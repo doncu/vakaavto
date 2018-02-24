df35a36ea672546ce020615a37f3d362cbc1094c
Ivan Krivosheev <i.krivosheev@cian.ru>
2018-02-24 18:00:35 +0300

    Add mail verification

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_recursive_files(src, dst):
 
 setuptools.setup(
     name='vakaavto',
-    version='1.0.7',
+    version='1.0.8',
     author="Ivan Krivosheev, Semen Doncu",
     author_email="py.krivosheev@gmail.com, doncusemen@gmail.com",
     description="Site for Vaka-Avto",
