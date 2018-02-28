1f2b8bfd11b5aeec9c8785a5a9791fbc8c895d82
s.dontsu <s.dontsu@rambler-co.ru>
2018-02-28 19:18:50 +0300

    fix index page words

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_recursive_files(src, dst):
 
 setuptools.setup(
     name='vakaavto',
-    version='1.1.1',
+    version='1.1.2',
     author="Ivan Krivosheev, Semen Doncu",
     author_email="py.krivosheev@gmail.com, doncusemen@gmail.com",
     description="Site for Vaka-Avto",
