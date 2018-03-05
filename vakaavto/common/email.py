289bdb388d6efe63a6c1d799a62e7df435a36e7c
Ivan <py.krivosheev@gmail.com>
2018-03-05 13:38:48 +0300

    Update email.py
    Fix render template

--- vakaavto/common/email.py
+++ vakaavto/common/email.py
@@ -1,15 +1,15 @@
 import smtplib
 from email.mime import text
 
-from flask import render_template_string
+from flask import render_template
 
 
 def send(host, log_pass, subject, from_, to, template, **kwargs):
     smtp = smtplib.SMTP_SSL(host=host, timeout=1)
     smtp.login(*log_pass)
 
-    email_string = render_template_string(template, **kwargs)
-    msg = text.MIMEText(email_string)
+    email_string = render_template(template, **kwargs)
+    msg = text.MIMEText(email_string, 'html')
     msg['Subject'] = subject
     msg['From'] = from_
     msg['To'] = to
