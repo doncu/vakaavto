4fd291e41516298f6ba2020adf47d200abb49207
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-12-06 01:53:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_recursive_files(src, dst):
 
 setuptools.setup(
     name='vakaavto',
-    version='2.0.5',
+    version='2.0.10',
     author="Ivan Krivosheev, Semen Doncu",
     author_email="py.krivosheev@gmail.com, doncusemen@gmail.com",
     description="Site for Vaka-Avto",
